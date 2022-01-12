de26a3c546b53b46f206b34efafbe688f118b390
zombifications <imma@terrorist.live>
2022-01-12 12:11:09 -0600

    banner because looks

--- email-osint/osint.py
+++ email-osint/osint.py
@@ -6,11 +6,25 @@ from modules import *
 
 regex = re.compile(r"([\w\-\.]+)(?:@)(\w[\w\-]+\.+[\w\-]+)", re.IGNORECASE)
 
+def banner() -> None:
+	print("""
+                      _ _             _       _   
+                     (_) |           (_)     | |  
+  ___ _ __ ___   __ _ _| |   ___  ___ _ _ __ | |_ 
+ / _ \ '_ ` _ \ / _` | | |  / _ \/ __| | '_ \| __|
+|  __/ | | | | | (_| | | | | (_) \__ \ | | | | |_ 
+ \___|_| |_| |_|\__,_|_|_|  \___/|___/_|_| |_|\__|
+                                                                                               
+                                                      
+""")
+
 def main():
 	parser = ArgumentParser(description="Email-OSINT")
 	parser.add_argument("email", help="Target email")
 	parser.add_argument("--no-breach", "-nb", default=False, required=False, action="store_true", dest="nobreach", help="Skip the breach check stage")
 	args = parser.parse_args()
+
+	banner()
 
 	if not bool(regex.match(args.email)):
 		print("That is not a valid email")
